5fa80aa29c199fe94bf17cb0fd0cfe3e7d99fa06
VictorS357 <victorfransousa357@gmail.com>
2026-06-08 15:16:08 -0300

    separate jsx components and css files

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -1,122 +1,8 @@
 import { useState, useEffect, useRef } from 'react'
-import { Chatbot } from 'supersimpledev'
-import RobotProfileImage from './assets/robot.png'
-import UserProfileImage from './assets/user.png'
-import LoadingGif from './assets/loading-spinner.gif'
+import { ChatInput  } from './components/ChatInput';
+import { ChatMessage } from './components/ChatMessage';
+import ChatMessages  from './components/ChatMessages';
 import './App.css'
-
-      function ChatInput({chatMessages, setChatMessages}) {
-        const [inputText, setInputText] = useState('');
-        const [isLoading, setIsLoading] = useState(false);
-
-        function saveInputText(event) {
-          setInputText(event.target.value);
-        }
-
-        async function sendMessage() {
-          if(isLoading || inputText === '') {
-            return;
-          }
-
-          setIsLoading(true);
-
-          const newChatMessages = ([
-            ...chatMessages,
-            {
-              message: inputText,
-              sender: 'user',
-              id: crypto.randomUUID()
-            }
-          ]);
-
-          setChatMessages(newChatMessages);
-
-          setInputText('');
-          setChatMessages([
-            ...newChatMessages,
-            {
-              message: <img className="loading" src={LoadingGif} />,
-              sender: 'robot',
-              id: crypto.randomUUID()
-            }
-          ]);
-
-
-          const response = await Chatbot.getResponseAsync(inputText);
-          setChatMessages([
-            ...newChatMessages,
-            {
-              message: response,
-              sender: 'robot',
-              id: crypto.randomUUID()
-            }
-          ]);
-          setIsLoading(false);
-        }
-
-        function keyDown(event) {
-          if(event.key === 'Enter') {
-            sendMessage();
-          } else if(event.key === 'Escape') {
-            setInputText('');
-          }
-        }
-
-        return (
-          <div className="chat-input-container">
-            <input 
-              placeholder="Send a message to Chatbot" 
-              size="30"
-              onChange={saveInputText} 
-              value={inputText}
-              onKeyDown={keyDown}
-              className="chat-input"
-            /> 
-            <button
-              onClick={sendMessage}
-              className="send-btn"
-            >Send</button>
-          </div>
-        ); // '<> </>' é um fragmento. Permite com que retorne um elemento da função sem criar uma div extra
-      }
-
-      function ChatMessage({message, sender}) {
-        //const message = props.message;
-        //const sender = props.sender;
-        //const {message} = props;
-        //const {sender} = props;
-
-        return (
-          <div className={sender === 'user' ? 'chat-message-user' : 'chat-message-robot'}>
-            {sender === 'robot' && (
-              <img src={RobotProfileImage} width="50" />
-            )}
-            <div className="chat-message-text">
-              {message}
-            </div>
-            {sender === 'user' && (
-              <img src={UserProfileImage} width="50" />
-            )}
-          </div>
-        );
-      }
-
-      function ChatMessages({chatMessages}) {
-        const chatMessagesRef = useAutoScroll(chatMessages);
-        return (
-          <div className="chat-message-container" ref={chatMessagesRef}>
-            {chatMessages.map(chatMessage => {
-              return(
-                <ChatMessage 
-                  message={chatMessage.message}
-                  sender={chatMessage.sender}
-                  key={chatMessage.id}
-                />
-              );
-            })}
-          </div>
-        );
-      }
 
       function useAutoScroll(dependencies) {
         const chatMessagesRef = useRef(null);
@@ -132,25 +18,25 @@ import './App.css'
       }
 
 function App() {
-        const [chatMessages, setChatMessages] = useState([]);
-        // const [chatMessages, setChatMessages] = array;
-        // const chatMessages = array[0]; Primeiro elemento do array de useState são os dados atuais salvos
-        // const setChatMessages = array[1]; O segundo elemento é uma função que atualiza os dados juntamente com o HTML (sempre colocar set no começo do nome)
-        
-        return (
-        <div className="app-container">
-          {chatMessages.length === 0 && (
-            <p className="welcome-p">Welcome to the chatbot project! Send a message using the textbox below.</p>
-          )}
-          <ChatMessages
-            chatMessages={chatMessages}
-          /> 
-          <ChatInput
-            chatMessages={chatMessages}
-            setChatMessages={setChatMessages}
-          />
-        </div>
-        );
-      }
+  const [chatMessages, setChatMessages] = useState([]);
+  // const [chatMessages, setChatMessages] = array;
+  // const chatMessages = array[0]; Primeiro elemento do array de useState são os dados atuais salvos
+  // const setChatMessages = array[1]; O segundo elemento é uma função que atualiza os dados juntamente com o HTML (sempre colocar set no começo do nome)
+  
+  return (
+  <div className="app-container">
+    {chatMessages.length === 0 && (
+      <p className="welcome-p">Welcome to the chatbot project! Send a message using the textbox below.</p>
+    )}
+    <ChatMessages
+      chatMessages={chatMessages}
+    /> 
+    <ChatInput
+      chatMessages={chatMessages}
+      setChatMessages={setChatMessages}
+    />
+  </div>
+  );
+}
 
 export default App
---
+++ chatbot-project/src/components/ChatInput.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+import { Chatbot } from 'supersimpledev'
+import LoadingGif from './assets/loading-spinner.gif'
+import './ChatInput.css';
+
+
+export function ChatInput({chatMessages, setChatMessages}) {
+    const [inputText, setInputText] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    function saveInputText(event) {
+        setInputText(event.target.value);
+    }
+
+    async function sendMessage() {
+        if(isLoading || inputText === '') {
+        return;
+        }
+
+        setIsLoading(true);
+
+        const newChatMessages = ([
+        ...chatMessages,
+        {
+            message: inputText,
+            sender: 'user',
+            id: crypto.randomUUID()
+        }
+        ]);
+
+        setChatMessages(newChatMessages);
+
+        setInputText('');
+        setChatMessages([
+        ...newChatMessages,
+        {
+            message: <img className="loading" src={LoadingGif} />,
+            sender: 'robot',
+            id: crypto.randomUUID()
+        }
+        ]);
+
+
+        const response = await Chatbot.getResponseAsync(inputText);
+        setChatMessages([
+        ...newChatMessages,
+        {
+            message: response,
+            sender: 'robot',
+            id: crypto.randomUUID()
+        }
+        ]);
+        setIsLoading(false);
+    }
+
+    function keyDown(event) {
+        if(event.key === 'Enter') {
+        sendMessage();
+        } else if(event.key === 'Escape') {
+        setInputText('');
+        }
+    }
+
+    return (
+        <div className="chat-input-container">
+        <input 
+            placeholder="Send a message to Chatbot" 
+            size="30"
+            onChange={saveInputText} 
+            value={inputText}
+            onKeyDown={keyDown}
+            className="chat-input"
+        /> 
+        <button
+            onClick={sendMessage}
+            className="send-btn"
+        >Send</button>
+        </div>
+    ); // '<> </>' é um fragmento. Permite com que retorne um elemento da função sem criar uma div extra
+}
